d1cd92b47173bdf57353436e1b392d583dfbbb52
Andrés Chyrnia <andreschyrnia@gmail.com>
2026-04-10 16:01:35 -0300

    KAN-42: Refactor types used accross layers

--- erp_back/src/modules/invoice/dto/response.rs
+++ erp_back/src/modules/invoice/dto/response.rs
@@ -1,12 +1,13 @@
 use serde::{Serialize,Deserialize};
 use chrono::NaiveDate;
+use rust_decimal::Decimal;
 
 use crate::modules::invoice::model;
 
 #[derive(Debug,Clone,Serialize,Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ClientResponse {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
     pub surname: String,
     pub ruc: String,
@@ -15,7 +16,7 @@ pub struct ClientResponse {
 #[derive(Debug,Clone,Serialize,Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SaleConditionResponse {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
 }
 
@@ -23,15 +24,15 @@ pub struct SaleConditionResponse {
 #[serde(rename_all = "camelCase")]
 pub struct LineItemResponse {
     pub product: LineProductResponse,
-    pub unit_cost: f64,
-    pub tax: u8,
-    pub quantity: u32,
+    pub unit_cost: Decimal,
+    pub tax: i8,
+    pub quantity: i32,
 }
 
 #[derive(Debug,Clone,Serialize,Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct LineProductResponse {
-    pub id: u32,
+    pub id: i32,
     pub description: String,
     pub code: String,
 }
@@ -39,15 +40,16 @@ pub struct LineProductResponse {
 #[derive(Debug,Clone,Serialize,Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct InvoiceResponse {
-    pub id: u32,
+    pub id: i32,
     pub invoice_number: String,
     pub created_at: NaiveDate,
     pub date: NaiveDate,
     pub expiration_date: NaiveDate,
-    pub total: f64,
-    pub total_paid: f64,
+    pub total: Decimal,
+    pub total_paid: Decimal,
     pub client: ClientResponse,
     pub sale_condition: SaleConditionResponse,
+    pub quote_id: Option<i32>,
     pub details: Vec<LineItemResponse>,
 }
 
@@ -65,12 +67,13 @@ impl From<model::InvoiceAggregate> for InvoiceResponse {
                 id: value.client.id,
                 name: value.client.name,
                 surname: value.client.surname,
-                ruc: value.client.ruc,
+                ruc: value.client.document,
             },
             sale_condition: SaleConditionResponse {
                 id: value.sale_condition.id,
                 name: value.sale_condition.name,
             },
+            quote_id: value.invoice.quote_id,
             details: value.invoice
                 .details
                 .into_iter()
@@ -85,12 +88,6 @@ impl From<model::InvoiceAggregate> for InvoiceResponse {
                     },
                 })
             .collect(),
-        }
-    }
-}
-
-                
-
         }
     }
 }
--- erp_back/src/modules/invoice/model.rs
+++ erp_back/src/modules/invoice/model.rs
@@ -1,46 +1,47 @@
 use chrono::NaiveDate;
+use rust_decimal::Decimal;
 //use crate::modules::quote::model::Quote;
 
 #[derive(Debug,Clone)]
 pub struct Invoice {
-    pub id: u32,
+    pub id: i32,
     pub invoice_number: String,
     pub created_at: NaiveDate,
     pub date: NaiveDate,
     pub expiration_date: NaiveDate,
-    pub total: f64,
-    pub total_paid: f64,
-    pub quote_id: Option<u32>,
-    pub client_id: u32,
-    pub sale_condition_id: u32,
+    pub total: Decimal,
+    pub total_paid: Decimal,
+    pub quote_id: Option<i32>,
+    pub client_id: i32,
+    pub sale_condition_id: i32,
     pub details: Vec<LineItem>,
 }
 
 #[derive(Debug,Clone)]
 pub struct Client {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
     pub surname: String,
-    pub ruc: String,
+    pub document: String,
 }
 
 #[derive(Debug,Clone)]
 pub struct SaleCondition {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
 }
 
 #[derive(Debug,Clone)]
 pub struct LineItem {
     pub product: LineProduct,
-    pub unit_cost: f64,
-    pub tax: u8,
-    pub quantity: u32,
+    pub unit_cost: Decimal,
+    pub tax: i8,
+    pub quantity: i32,
 }
 
 #[derive(Debug,Clone)]
 pub struct LineProduct {
-    pub id: u32,
+    pub id: i32,
     pub description: String,
     pub code: String,
 }
