829d155547c8adbb9647d2594ae7b35230f18aac
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-10 02:40:44 -0300

    KAN-41: Add CreateClientDto for POST /clients

---
+++ erp_back/src/modules/client/dto/create.rs
@@ -0,0 +1,24 @@
+//What POST receives - no id, no currentCredit
+use chrono::NaiveDate;
+use rust_decimal::Decimal;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]  //birthDate, creditLimit, etc
+pub struct CreateClientDto {
+    pub name: String,
+    pub surname: String,
+    pub ruc: String,
+    pub address: String,
+    pub email: String,
+    pub birth_date: Option<NaiveDate>,
+    pub credit_limit: Decimal,
+    pub phones: Vec<CreatePhoneDto>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CreatePhoneDto {
+    pub phone_number: String,
+    pub is_emergency: bool,
+}
