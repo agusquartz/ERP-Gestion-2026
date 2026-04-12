6aae8d4a26d3b61c2323579beaa791b28dab4716
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-12 19:02:47 -0300

    KAN-41: Add patch quote DTOs

---
+++ erp_back/src/modules/quote/dto/update.rs
@@ -0,0 +1,31 @@
+use chrono::NaiveDate;
+use serde::Deserialize;
+
+// ------------------------------------------------------------
+// PatchQuoteDto
+// This DTO defines the payload expected by PATCH /quotes/:id.
+// All fields are optional because PATCH is partial.
+// ------------------------------------------------------------
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PatchQuoteDto {
+    pub client_id: Option<i32>,
+    pub status_id: Option<i32>,
+    pub created_at: Option<NaiveDate>,
+    pub details: Option<Vec<PatchQuoteDetailDto>>,
+}
+
+// ------------------------------------------------------------
+// PatchQuoteDetailDto
+// This DTO is used when quote details are replaced.
+// The list is optional, but each item must be valid.
+// ------------------------------------------------------------
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PatchQuoteDetailDto {
+    pub product_id: i32,
+    pub quantity: i32,
+    pub unit_cost: f64,
+}
