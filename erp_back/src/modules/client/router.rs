fbdb3658a50691fd956b827bc72b9340bdfdde34
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-14 15:03:23 -0300

    KAN-41: Replace client/:id to client/{id}

--- erp_back/src/modules/client/router.rs
+++ erp_back/src/modules/client/router.rs
@@ -9,7 +9,7 @@ use crate::modules::auth::middleware::auth::protect_routes;
 pub fn client_router() -> Router {
    let protected =  Router::new()
         .route("/clients", get(handler::get_clients).post(handler::create_client))  
-        .route("/clients/:id", get(handler::get_client_by_id).patch(handler::patch_client));
+        .route("/clients/{id}", get(handler::get_client_by_id).patch(handler::patch_client));
 
     protect_routes(protected)
 }
