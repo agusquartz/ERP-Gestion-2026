f63009acd3957aedb99a7fc8efe84ee65793aa57
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-15 15:08:34 -0300

    KAN-41(Refactor): Remove details update from patch DTO

--- erp_back/src/modules/quote/dto/update.rs
+++ erp_back/src/modules/quote/dto/update.rs
@@ -1,30 +1,13 @@
-use chrono::NaiveDate;
-use serde::Deserialize;
+//! Patch Quote DTO
+//!
+//! All fields optional because this is partial update.
 
-// ------------------------------------------------------------
-// PatchQuoteDto
-// This DTO defines the payload expected by PATCH /quotes/:id.
-// All fields are optional because PATCH is partial.
-// ------------------------------------------------------------
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PatchQuoteDto {
     pub client_id: Option<i32>,
     pub status_id: Option<i32>,
-    pub created_at: Option<NaiveDate>,
-    pub details: Option<Vec<PatchQuoteDetailDto>>,
-}
-
-// ------------------------------------------------------------
-// PatchQuoteDetailDto
-// This DTO is used when quote details are replaced.
-// The list is optional, but each item must be valid.
-// ------------------------------------------------------------
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct PatchQuoteDetailDto {
-    pub product_id: i32,
-    pub quantity: i32,
+    pub created_at: Option<String>,
 }
