940580135104c01d5ac8e23d939a6c4cfa4e8f02
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-15 22:37:59 -0300

    KAN-41(Refactor): Replace String errors with DbError in service layer

--- erp_back/src/modules/quote/service.rs
+++ erp_back/src/modules/quote/service.rs
@@ -13,12 +13,17 @@ use crate::modules::quote::{
     repository,
     mapper,
 };
+
+use crate::db_config::DbError;
+
 pub async fn create_quote(
     dto: CreateQuoteDto,
-) -> Result<QuoteResponseDto, String> {
+) -> Result<QuoteResponseDto, crate::db_config::DbError> {
 
     if dto.details.is_empty() {
-        return Err("Quote must contain at least one detail".to_string());
+        return Err(DbError::Other(
+        "Quote must contain at least one detail".to_string()
+        ));
     }
 
     let quote = repository::create_quote(dto).await?;
@@ -28,9 +33,9 @@ pub async fn create_quote(
 
 
 /// Get quote by id
-pub async fn get_quote_by_id(id: i32) -> Result<QuoteResponseDto, String> {
+pub async fn get_quote_by_id(id: i32) -> Result<QuoteResponseDto, crate::db_config::DbError> {
     let quote = repository::get_quote_by_id(id).await?
-        .ok_or("Quote not found".to_string())?;
+        .ok_or(DbError::NotFound)?;
 
     Ok(mapper::quote_with_details_to_response(quote))
 }
@@ -40,7 +45,7 @@ pub async fn get_quotes(
     contains: Option<String>,
 ) -> Result<Vec<QuoteResponseDto>, crate::db_config::DbError> {
 
-    let quotes = repository::get_quotes(contains.as_deref()).await?;
+    let quotes = repository::get_quotes(contains).await?;
 
     Ok(quotes
         .into_iter()
