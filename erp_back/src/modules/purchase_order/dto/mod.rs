90748f88f2866e6278cb00ba2f311d2050594ea7
Andrés Chyrnia <andreschyrnia@gmail.com>
2026-04-26 19:48:19 -0300

    KAN-52: Add handlers and ListQuery mini dto

--- erp_back/src/modules/purchase_order/dto/mod.rs
+++ erp_back/src/modules/purchase_order/dto/mod.rs
@@ -1,3 +1,10 @@
+use serde::{ Deserialize, Serialize };
+
 pub mod create;
 pub mod update;
 pub mod response;
+
+#[derive(Debug,Clone,Serialize,Deserialize)]
+pub struct PurchaseOrderListQuery {
+    pub contains: Option<String>,
+}
