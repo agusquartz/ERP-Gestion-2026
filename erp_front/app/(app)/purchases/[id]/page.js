edd7770f400fb80274f8b9509a168257c73ae7e9
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-04-28 01:29:18 -0300

    KAN-49(Refactor): Simplify Page component by removing wrapper div and using React.use()

--- erp_front/app/(app)/purchases/[id]/page.js
+++ erp_front/app/(app)/purchases/[id]/page.js
@@ -1,9 +1,7 @@
 import PurchaseOrderPage from "@/modules/purchases/pages/PurchaseOrderPage";
+import { use } from "react";
 
 export default function Page({ params }) {
-  return (
-    <div className="p-2 h-full">
-      <PurchaseOrderPage orderId={params.id} />
-    </div>
-  );
+  const { id } = use(params);
+  return <PurchaseOrderPage orderId={id} />;
 }
