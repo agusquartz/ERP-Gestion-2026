fe655b038beb025bbcde82fa74dc7841bf72155d
agusquartz <agusquartz@gmail.com>
2026-05-03 18:40:17 -0300

    KAN-46: pass quote id to new sale page

--- erp_front/app/(app)/sales/new/page.js
+++ erp_front/app/(app)/sales/new/page.js
@@ -1,2 +1,9 @@
 import NewSalePage from "@/modules/sales/pages/NewSalePage";
-export default NewSalePage;
+
+export default async function Page({ searchParams }) {
+  const params = await searchParams;
+
+  console.log("searchParams en page.js:", params);
+
+  return <NewSalePage quoteId={params?.quote_id ?? null} />;
+}
