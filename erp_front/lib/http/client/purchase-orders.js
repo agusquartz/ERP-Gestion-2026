05fe96d6af83c4859457b6ddf12e6d5246b130e4
Andrés Chyrnia <andreschyrnia@gmail.com>
2026-05-08 13:17:10 -0300

    KAN-56: Add view purchase order screen

--- erp_front/lib/http/client/purchase-orders.js
+++ erp_front/lib/http/client/purchase-orders.js
@@ -7,5 +7,19 @@ export function getPurchaseOrdersByQuery(q) {
 }
 
 export function getPurchaseOrderById(id) {
-	//TODO
+	return clientRequest(`/purchases/purchase-orders/${id}`, {
+		method: "GET",
+	});
+}
+
+export function patchPurchaseOrder(id, payload) {
+  // We use underscores to match your Rust route: /purchases/purchase_orders/{id}
+  return clientRequest(`/purchases/purchase_orders/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(payload),
+  });
+}
+
+export function cancelPurchaseOrder(id, payload) {
+	return patchPurchaseOrder(id, payload);
 }
