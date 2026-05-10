fa901a3066617a0c8c292eea2b6b6d69c6e606a7
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-05-09 23:45:11 -0300

    KAN-57: Add purchase_invoice module and integrate its router

--- erp_back/src/main.rs
+++ erp_back/src/main.rs
@@ -112,6 +112,7 @@ async fn main() {
         .merge(modules::purchase_request::router::list_purchase_request_router())
         .merge(modules::purchase_invoice::router::purchase_invoice_router())
         .merge(modules::purchase_request::router::list_purchase_request_router())
+        .merge(modules::purchase_invoice::router::purchase_invoice_router())
         .layer(CookieManagerLayer::new())
         .layer(cors);
 
