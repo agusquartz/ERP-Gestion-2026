742196579447696182a0673dea983aa3a1bc59f4
Bianca Acuña <biancaacuna34@gmail.com>
2026-05-18 10:51:12 -0300

    KAN-60: attach POST action to /return-notes endpoint route

--- erp_back/src/modules/return_notes/router.rs
+++ erp_back/src/modules/return_notes/router.rs
@@ -7,6 +7,6 @@ use crate::modules::return_notes::handler;
 
 pub fn routes() -> Router {
     Router::new()
-        .route("/return-notes", get(handler::list_return_notes))
+        .route("/return-notes",get(handler::list_return_notes).post(handler::create_return_note))
         .route("/return-notes/{id}", get(handler::get_return_note_by_id))
 }
