c622a0cf082ea6693f753a9d017e5750d4391de4
aylenarzamendia <aylenarzamendia@fiuni.edu.py>
2026-05-27 22:26:29 -0300

    KAN-73: change the bad router

--- erp_back/src/modules/sales_reports/router.rs
+++ erp_back/src/modules/sales_reports/router.rs
@@ -7,5 +7,5 @@ use crate::modules::sales_reports::handler;
 pub fn sales_router() -> Router {
     Router::new()
         // La URL final en el navegador quedará: /api/sales/reports?
-        .route("/reports/sales", get(handler::get_sales_report))
+        .route("/sales/reports", get(handler::get_sales_report))
 }
